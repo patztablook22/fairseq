fixed ewc_consolidate fisher computation

my_scripts/create_bitedit_examples.py
@@ -16,43 +16,43 @@ def binarize(n, mapping):
     return res
 
 
-def create_id_example(x, mapping, tok=None, n=None):
+def create_id_example(x, tok=None, n=None):
     return (x, "-", "-")
 
 
-def create_push_example(x, mapping, tok, n=1):
+def create_push_example(x, tok, n=1):
     # push
     return (x + [tok] * n, tok, str(n))
 
 
-def create_pop_example(x, mapping, tok=None, n=1):
+def create_pop_example(x, tok=None, n=1):
     # pop
     return (x[:-n], "-", str(n))
 
 
-def create_unshift_example(x, mapping, tok, n=1):
+def create_unshift_example(x, tok, n=1):
     # unshift
     return ([tok] * n + x, tok, str(n))
 
 
-def create_shift_example(x, mapping, tok=None, n=1):
+def create_shift_example(x, tok=None, n=1):
     # shift
     return (x[n:], "-", str(n))
 
 
-def create_remove_tokens_example(x, mapping, tok, n=None):
+def create_remove_tokens_example(x, tok, n=None):
     # remove
     return ([e for e in x if e != tok], tok, "-")
 
 
-def create_reverse_example(x, mapping, tok=None, n=None):
+def create_reverse_example(x, tok=None, n=None):
     # reverse
     y = x.copy()
     y.reverse()
     return (y, "-", "-")
 
 
-def create_duplicate_example(x, mapping, tok=None, n=None):
+def create_duplicate_example(x, tok=None, n=None):
     # duplicate
     return (x + x, "-", "-")
 
@@ -68,28 +68,33 @@ def create_flip_reverse_example(x, mapping, tok=None, n=None):
     y.reverse()
     return (y, "-", "-")
 
-TASK_MAP = {
-    "id": create_id_example,
-    "push": create_push_example,
-    "pop": create_pop_example,
-    "shift": create_shift_example,
-    "unshift": create_unshift_example,
-    "remove": create_remove_tokens_example,
-    "reverse": create_reverse_example,
-    "duplicate": create_duplicate_example,
-    "flip": create_flip_example,
-    "flip-reverse": create_flip_reverse_example
-}
-
 
 def main(args):
-    if args.task not in TASK_MAP:
-        raise ValueError("Undefined Task: '{}'".format(args.task))
-
-    mapping = {
+    binary_mapping = {
         '0' : args.zero_char,
         '1' : args.one_char
     }
+
+    flip_mapping = {
+        args.zero_char : args.one_char,
+        args.one_char : args.zero_char,
+    }
+
+    TASK_MAP = {
+        "id": create_id_example,
+        "push": create_push_example,
+        "pop": create_pop_example,
+        "shift": create_shift_example,
+        "unshift": create_unshift_example,
+        "remove": create_remove_tokens_example,
+        "reverse": create_reverse_example,
+        "duplicate": create_duplicate_example,
+        "flip": lambda x, tok, n: create_flip_example(x, flip_mapping, tok, n),
+        "flip-reverse": lambda x, tok, n: create_flip_reverse_example(x, flip_mapping, tok, n),
+    }
+
+    if args.task not in TASK_MAP:
+        raise ValueError("Undefined Task: '{}'".format(args.task))
 
     np.random.seed(args.seed)
     rng = default_rng(args.seed)
@@ -99,13 +104,13 @@ def main(args):
         size=args.n_examples, replace=False)
     x_all += 2 ** args.min_n_bits
     for i, x in enumerate(x_all):
-        x_bin = binarize(x, mapping)
+        x_bin = binarize(x, binary_mapping)
 
         fn = TASK_MAP[args.task]
         if i % 2 == 0:
-            y_bin, tok, n = fn(x_bin, mapping, args.zero_char, args.n)
+            y_bin, tok, n = fn(x_bin, args.zero_char, args.n)
         else:
-            y_bin, tok, n = fn(x_bin, mapping, args.one_char, args.n)
+            y_bin, tok, n = fn(x_bin, args.one_char, args.n)
         print("{} {} {}\t{}\t{}".format(
             args.task, tok, n, " ".join(x_bin), " ".join(y_bin)))
 