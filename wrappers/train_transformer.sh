03b016ef9d4fca02ac35421f8eb976a38b04bd4a
varisd <varis@ufal.mff.cuni.cz>
2022-09-07 16:58:46 +0200

    fixed modules/modular.py and updated training wrappers

--- wrappers/train_transformer.sh
+++ wrappers/train_transformer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#~/bin/bash
 set -e
 
 JOB_PRIORITY=-90
@@ -6,21 +6,28 @@ GPUMEM="11g"
 
 EXPDIR=
 EVAL_DIR=
-TASKS="id push pop shift unshift reverse"
-VALID_TASKS=$TASKS
+TASKS="czeng"
+#TASKS="id.15 push.15 pop.15 shift.15 unshift.15 reverse.15"
+VALID_TASKS="bpe.newstest"
+#VALID_TASKS=$TASKS
+
+SRC=en
+TGT=cs
 
 # General Architecture Details
-EMB_SIZE=128
+EMB_SIZE=512
 FFN_SIZE=$(expr 4 \* $EMB_SIZE)
 ATT_HEADS=8
-DEPTH=1
+DEPTH=6
 SHARED_DICT_OPT=
 
 # Training Reset
+INIT_CKPT=
 RESET_OPTIMIZER_OPT=
 
 # Training Details
 RANDOM_SEED=42
+INITIAL_EPOCH=0  # used with initial checkpoint (which have non-zero epoch number)
 EPOCHS=100
 LABEL_SMOOTHING=0.1
 MAX_TOKENS=4096
@@ -32,10 +39,22 @@ PATIENCE=0
 KEEP_N_CHECKPOINTS=1
 SAVE_EVERY_N_UPDATES=0
 
+ADAM_BETA_1=0.9
+ADAM_BETA_2=0.98
+
 # Validation - Beam Search Details
-VALID_BEAM_SIZE=5
+VALID_BEAM_SIZE=1
 VALID_MAX_LEN_A=1.2
 VALID_MAX_LEN_B=10
+VALID_LENPEN=0.6
+
+BEST_METRIC="bleu"  # bleu, ter, accuracy
+MINIMIZE_METRIC="--maximize-best-checkpoint-metric"  # We want maximization to be default
+
+EVAL_SCRIPT=process_checklist.sh  # Validation wrapper (called at the end of each task training)
+
+# Parameter freeze (for consecutive tasks)
+FREEZE_PARAMS=
 
 HELP=1
 while [[ $# -gt 0 ]]; do
@@ -49,6 +68,14 @@ case $key in
         EVAL_DIR="$2"
         shift
     ;;
+    --src)
+        SRC="$2"
+        shift
+    ;;
+    --tgt)
+        TGT="$2"
+        shift
+    ;;
     -r|--random-seed)
         RANDOM_SEED="$2"
         shift
@@ -57,12 +84,16 @@ case $key in
         TASKS="$2"
         shift
     ;;
-    --valid-tasks)
+    -v|--valid-tasks)
         VALID_TASKS="$2"
         shift
     ;;
     --epochs)
         EPOCHS="$2"
+        shift
+    ;;
+    --initial-epoch)
+        INITIAL_EPOCH="$2"
         shift
     ;;
     --emb-size)
@@ -121,8 +152,39 @@ case $key in
         VALID_MAX_LEN_B="$2"
         shift
     ;;
+    --valid-lenpen)
+        VALID_LENPEN="$2"
+        shift
+    ;;
+    --adam-beta-1)
+        ADAM_BETA_1="$2"
+        shift
+    ;;
+    --adam-beta-2)
+        ADAM_BETA_2="$2"
+        shift
+    ;;
+    --freeze-params)
+        FREEZE_PARAMS="$2"
+        shift
+    ;;
+    --eval-script)
+        EVAL_SCRIPT="$2"
+        shift
+    ;;
+    --best-metric)
+        BEST_METRIC="$2"
+        shift
+    ;;
+    --minimize-metric)
+        MINIMIZE_METRIC=""
+    ;;
     --save-every-n)
         SAVE_EVERY_N_UPDATES="$2"
+        shift
+    ;;
+    --init-ckpt)
+        INIT_CKPT="$2"
         shift
     ;;
     --reset-optimizer)
@@ -151,29 +213,39 @@ done
 
 MODEL_DIR="$EXPDIR/transformer"
 MODEL_DIR="$MODEL_DIR.seed-$RANDOM_SEED"
-MODEL_DIR="$MODEL_DIR.warmup-$WARMUP"
-MODEL_DIR="$MODEL_DIR.clip-norm-$CLIP_NORM"
+#MODEL_DIR="$MODEL_DIR.tasks-`echo $TASKS | sed 's/ /-/g'`"
+#MODEL_DIR="$MODEL_DIR.warmup-$WARMUP"
+#MODEL_DIR="$MODEL_DIR.clip-norm-$CLIP_NORM"
 MODEL_DIR="$MODEL_DIR.emb-size-$EMB_SIZE"
-#MODEL_DIR="$MODEL_DIR.ffn-size-$FFN_SIZE"
 MODEL_DIR="$MODEL_DIR.att-heads-$ATT_HEADS"
 MODEL_DIR="$MODEL_DIR.depth-$DEPTH"
+MODEL_DIR="$MODEL_DIR.smooth-$LABEL_SMOOTHING"
+#MODEL_DIR="$MODEL_DIR.patience-$PATIENCE"
 MODEL_DIR="$MODEL_DIR.lr-$LR"
 #MODEL_DIR="$MODEL_DIR.max-tokens-$MAX_TOKENS"
 [[ -z "$RESET_OPTIMIZER_OPT" ]] || MODEL_DIR="$MODEL_DIR.reset-optim"
 
 [[ -d $MODEL_DIR ]] && rm -r $MODEL_DIR
-mkdir $MODEL_DIR
+mkdir $MODEL_DIR && mkdir $MODEL_DIR/checkpoints
 echo $TASKS | sed 's/ /->/g' > $MODEL_DIR/TASKS
+echo $FREEZE_PARAMS > $MODEL_DIR/FREEZE_PARAMS
 
 ckpt_opt=
-epochs=$EPOCHS
-valid_sets=`echo $VALID_TASKS | sed 's/ /.15.valid,/g;s/$/.15.valid/'`
+[[ -e "$INIT_CKPT" ]] && ckpt_opt="--restore-file $INIT_CKPT" && cp $INIT_CKPT $MODEL_DIR/checkpoints/checkpoint_last.pt
+epochs=`expr $INITIAL_EPOCH + $EPOCHS`
+valid_sets=`echo $VALID_TASKS | sed 's/ /.valid,/g;s/$/.valid/'`
 for current_task in $TASKS; do
     echo Training $current_task...
+
+     PARAM_FREEZE_OPT=
+    if [[ -e "$MODEL_DIR/checkpoints/checkpoint_last.pt" ]] || [[ -e "$INIT_CKPT" ]]; then
+        [[ -n $FREEZE_PARAMS ]] && PARAM_FREEZE_OPT="--parameter-freeze-substr '$FREEZE_PARAMS'"
+    fi
+
     jid=$(qsubmit \
         --queue="gpu-troja.q" \
         --logdir=$MODEL_DIR/logs \
-        --jobname=tformer_bit_base \
+        --jobname=$current_task.train \
         --mem=25g \
         --cores=4 \
         --gpumem=$GPUMEM \
@@ -181,49 +253,51 @@ for current_task in $TASKS; do
         --gpus=1 "source $HOME/python-virtualenv/fairseq-env/bin/activate && \
             export CUDA_LAUNCH_BLOCKING=1 && \
             python train.py \
-            $EXPDIR/data \
-            -s x \
-            -t y \
-            --seed $RANDOM_SEED \
-            --task translation \
-            --arch transformer \
-            --share-decoder-input-output-embed \
-            $SHARED_DICT_OPT \
-            --train-subset ${current_task}.15.train \
-            --valid-subset $valid_sets \
-            $ckpt_opt \
-            $RESET_OPTIMIZER_OPT \
-            --optimizer adam \
-            --adam-betas '(0.9, 0.98)' \
-            --clip-norm $CLIP_NORM \
-            --patience $PATIENCE \
-            --max-epoch $epochs \
-            --lr $LR \
-            --lr-scheduler inverse_sqrt \
-            --warmup-updates $WARMUP \
-            --dropout $DROPOUT \
-            --weight-decay 0.0001 \
-            --criterion label_smoothed_cross_entropy \
-            --label-smoothing $LABEL_SMOOTHING \
-            --max-tokens $MAX_TOKENS \
-            --eval-acc \
-            --eval-ter \
-            --eval-bleu \
-            --eval-bleu-args '{\"beam\": $VALID_BEAM_SIZE, \"max_len_a\": $VALID_MAX_LEN_A, \"max_len_b\": $VALID_MAX_LEN_B}' \
-            --eval-bleu-detok moses \
-            --eval-bleu-remove-bpe \
-            --eval-bleu-print-samples \
-            --best-checkpoint-metric ter \
-            --tensorboard-logdir $MODEL_DIR \
-            --save-dir $MODEL_DIR/checkpoints \
-            --keep-last-epochs $KEEP_N_CHECKPOINTS \
-            --encoder-attention-heads $ATT_HEADS \
-            --decoder-attention-heads $ATT_HEADS \
-            --encoder-layers $DEPTH \
-            --decoder-layers $DEPTH \
-            --encoder-embed-dim $EMB_SIZE \
-            --encoder-ffn-embed-dim $FFN_SIZE \
-            --save-interval-updates $SAVE_EVERY_N_UPDATES")
+                $EXPDIR/data \
+                -s $SRC \
+                -t $TGT \
+                --seed $RANDOM_SEED \
+                --task translation \
+                --arch transformer \
+                --share-decoder-input-output-embed \
+                $SHARED_DICT_OPT \
+                --train-subset ${current_task}.train \
+                --valid-subset $valid_sets \
+                $ckpt_opt \
+                $RESET_OPTIMIZER_OPT \
+                --optimizer adam \
+                --adam-betas '($ADAM_BETA_1, $ADAM_BETA_2)' \
+                --clip-norm $CLIP_NORM \
+                --patience $PATIENCE \
+                --max-epoch $epochs \
+                --lr $LR \
+                --lr-scheduler inverse_sqrt \
+                --warmup-updates $WARMUP \
+                --dropout $DROPOUT \
+                --weight-decay 0.0001 \
+                --criterion label_smoothed_cross_entropy \
+                --label-smoothing $LABEL_SMOOTHING \
+                --max-tokens $MAX_TOKENS \
+                --eval-acc \
+                --eval-ter \
+                --eval-bleu \
+                --eval-bleu-args '{\"beam\": $VALID_BEAM_SIZE, \"max_len_a\": $VALID_MAX_LEN_A, \"max_len_b\": $VALID_MAX_LEN_B, \"lenpen\": $VALID_LENPEN}' \
+                --eval-bleu-detok moses \
+                --eval-bleu-remove-bpe \
+                --eval-bleu-print-samples \
+                --best-checkpoint-metric $BEST_METRIC \
+                $MINIMIZE_METRIC \
+                --tensorboard-logdir $MODEL_DIR \
+                --save-dir $MODEL_DIR/checkpoints \
+                --keep-last-epochs $KEEP_N_CHECKPOINTS \
+                --encoder-attention-heads $ATT_HEADS \
+                --decoder-attention-heads $ATT_HEADS \
+                --encoder-layers $DEPTH \
+                --decoder-layers $DEPTH \
+                --encoder-embed-dim $EMB_SIZE \
+                --encoder-ffn-embed-dim $FFN_SIZE \
+                $PARAM_FREEZE_OPT \
+                --save-interval-updates $SAVE_EVERY_N_UPDATES")
 
     jid=`echo $jid | cut -d" " -f3`
     echo Waiting for $jid...
@@ -231,14 +305,26 @@ for current_task in $TASKS; do
         sleep 15
         qstat | grep $jid > /dev/null || break
     done
+
+    # Save the last ckpt for current task and set it as initial
+    # ckpt for the next iteration
     cp $MODEL_DIR/checkpoints/checkpoint_last.pt \
         $MODEL_DIR/checkpoints/checkpoint_$current_task.pt
     ckpt_opt="--restore-file $MODEL_DIR/checkpoints/checkpoint_$current_task.pt"
-    bash process_checklist.sh \
-        -t $current_task \
-        --expdir $MODEL_DIR \
-        --tasks "$VALID_TASKS $current_task" \
-        --eval-dir $EVAL_DIR &
+
+    # Validate
+    echo "Validating using $EVAL_SCRIPT..."
+    for t in valid test; do
+        bash $EVAL_SCRIPT \
+            -t $current_task \
+            --src $SRC \
+            --tgt $TGT \
+            --expdir $MODEL_DIR \
+            --tasks "`echo $VALID_TASKS | sed 's/\.[^. ]* / /g;s/\.[^. ]*$//'`" \
+            --eval-dir $EVAL_DIR \
+            --eval-prefix "$t" \
+        | tee -a $MODEL_DIR/logs/$current_task.eval.log &
+    done
 
     epochs=`expr $epochs + $EPOCHS`
 done
