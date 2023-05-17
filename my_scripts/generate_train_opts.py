f25b2e72ac29e648636a3261af8788774d130f1f
varisd <varis@ufal.mff.cuni.cz>
2023-05-17 15:43:37 +0200

    removed personal-experiment-related directories (should not be in forked master)

--- my_scripts/generate_train_opts.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import sys, random
-
-for i in range(int(sys.argv[1])):
-    print('--lr_{:.5f}_--warmup_{}'.format(float(random.uniform(1e-4, 9e-4)), int(random.uniform(2000, 10000))))
